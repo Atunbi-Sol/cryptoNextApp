e0378ef88e1cc23da75a15c0e0c7f688a6639701
atunbi <damilareatunbi@gmail.com>
2023-06-24 16:35:32 +0100

    path route

--- pages/[id].js
+++ pages/[id].js
@@ -9,62 +9,52 @@ const Coin = ({ coins }) => {
       <section style={{ background: "#eee" }}>
         <div className="container">
           <h1>Coin Details</h1>
-          <div>
-            {coins?.map((coin, id) => {
-              return (
-                <div className="--flex-start --column-reverse" key={id}>
-                  <div className="--card --card-blue --mr">
-                    <h4 className="--color-danger">~{coin.name}</h4>
-                    <h4>~ Price${formatNumbers(coin.price.toFixed(2))}</h4>
-                    <h4>~ Rank: {coin.rank}</h4>
-                    <ul className="coin-details">
-                      <li>
-                        <span> Change in 1Hr</span>
-                        <span className={checkPrice(coin.priceChange1h)}>{coin.priceChange1h}</span>
-                      </li>
-                      <li>
-                        <span> Change in 24Hr</span>
-                        <span className={checkPrice(coin.priceChange1d)}>{coin.priceChange1d}</span>
-                      </li>
-                      <li>
-                        <span> Change in 1week</span>
-                        <span className={checkPrice(coin.priceChange1w)}>{coin.priceChange1w}</span>
-                      </li>
-                      <li>
-                        <span>Market Cap:</span>
-                        <span>${formatNumbers(coin.marketCap.toFixed(2))}</span>
-                      </li>
-                      <li>
-                        <span>Total Supply:</span>
-                        <span>{formatNumbers(coin.totalSupply)}</span>
-                      </li>
-                      <li>
-                        <span>Website :</span>
-                        <span>
-                          <a href={coin.websiteUrl} target="_blank" rel="noreferrer" className="--color-primary">
-                            {coin.websiteUrl
-                              .replace("http://www.", "")
-                              .replace("https://www.", "")
-                              .replace("http://", "")
-                              .replace("/", "")}
-                          </a>
-                        </span>
-                      </li>
-                    </ul>
-                    <div className="--card --bg-primary">
-                      <div className="--flex-start --align-center">
-                        <Image src={coin.icon} alt={coin.name} width="50" height={50} />
-                        &nbsp; &nbsp;
-                        <span>
-                          <p className="--text-ligt"> {coin.symbol}</p>
-                          <h2 className="--text-ligt"> {coin.name}</h2>
-                        </span>
-                      </div>
-                    </div>
-                  </div>
+          <div className="--flex-start --column-reverse" key={id}>
+            <div className="--card --card-blue --mr">
+              <h4 className="--color-danger">~{coin.name}</h4>
+              <h4>~ Price${formatNumbers(coin.price.toFixed(2))}</h4>
+              <h4>~ Rank: {coin.rank}</h4>
+              <ul className="coin-details">
+                <li>
+                  <span> Change in 1Hr</span>
+                  <span className={checkPrice(coin.priceChange1h)}>{coin.priceChange1h}</span>
+                </li>
+                <li>
+                  <span> Change in 24Hr</span>
+                  <span className={checkPrice(coin.priceChange1d)}>{coin.priceChange1d}</span>
+                </li>
+                <li>
+                  <span> Change in 1week</span>
+                  <span className={checkPrice(coin.priceChange1w)}>{coin.priceChange1w}</span>
+                </li>
+                <li>
+                  <span>Market Cap:</span>
+                  <span>${formatNumbers(coin.marketCap.toFixed(2))}</span>
+                </li>
+                <li>
+                  <span>Total Supply:</span>
+                  <span>{formatNumbers(coin.totalSupply)}</span>
+                </li>
+                <li>
+                  <span>Website :</span>
+                  <span>
+                    <a href={coin.websiteUrl} target="_blank" rel="noreferrer" className="--color-primary">
+                      {coin.websiteUrl.replace("http://www.", "").replace("https://www.", "").replace("http://", "").replace("/", "")}
+                    </a>
+                  </span>
+                </li>
+              </ul>
+              <div className="--card --bg-primary">
+                <div className="--flex-start --align-center">
+                  <Image src={coin.icon} alt={coin.name} width="50" height={50} />
+                  &nbsp; &nbsp;
+                  <span>
+                    <p className="--text-ligt"> {coin.symbol}</p>
+                    <h2 className="--text-ligt"> {coin.name}</h2>
+                  </span>
                 </div>
-              );
-            })}
+              </div>
+            </div>
           </div>
         </div>
       </section>
@@ -72,32 +62,32 @@ const Coin = ({ coins }) => {
   );
 };
 
-// export const getStaticPaths = async ({ coins, setCoins }) => {
-//   const id = context.params.id;
-//   const res = await axios.get("https://api.coinstats.app/public/v1/coins/" + i);
-//   setCoins(res.data.coins);
+export const getStaticPaths = async ({ coins, setCoins }) => {
+  const id = context.params.id;
+  const res = await axios.get("https://api.coinstats.app/public/v1/coins/" + i);
+  setCoins(res.data.coins);
 
-//   const paths = coins.coins.map((coin) => {
-//     return {
-//       params: {
-//         id: coin.id.toString(),
-//       },
-//     };
-//   });
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
-// export const fetchData = async (context, { coins, setCoins }) => {
-//   const id = context.params.id;
-//   const res = await axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10");
-//   setCoins(res.data.coins);
-//   return {
-//     props: {
-//       coin: coins,
-//     },
-//   };
-// };
+  const paths = coins.coins.map((coin) => {
+    return {
+      params: {
+        id: coin.id.toString(),
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+export const fetchData = async (context, { coins, setCoins }) => {
+  const id = context.params.id;
+  const res = await axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10");
+  setCoins(res.data.coins);
+  return {
+    props: {
+      coin: coins,
+    },
+  };
+};
 
 export default Coin;
